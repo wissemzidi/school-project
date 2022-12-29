211056e079eb784015bac030953bb988f84cc1a6
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-12-29 20:54:30 +0100

    adding the hero section and sidebar and footer

---
+++ js/home.js
@@ -0,0 +1,29 @@
+function toggleAside() {
+  let aside = document.getElementById("aside");
+  let asideBtn = document.getElementById("toggle-aside");
+
+  if (aside.className === "") {
+    console.log("closing..");
+    aside.style.animation = "slideOut-left .6s ease forwards";
+    setTimeout(() => {
+      aside.className = "close";
+    }, 600);
+    asideBtn.style.transform = "rotateY(0deg)";
+  } else {
+    console.log("opening..");
+    aside.className = "";
+    aside.style.animation = "slideIn-right .7s ease forwards";
+    asideBtn.style.transform = "rotateY(180deg)";
+  }
+}
+
+function hideLoader() {
+  setTimeout(() => {
+    let loader = document.getElementById("spinner-container");
+    loader.style.opacity = 0;
+    document.documentElement.style.overflow = "auto";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 800);
+  }, 1000);
+}
